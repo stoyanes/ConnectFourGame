d0f52df10d92a51a50b8f473c944a6fda4f9b54a
Stoyan Stoyanov <djstoxer@gmail.com>
2013-01-02 21:05:20 +0200

    first commit. Nothing special about it

---
+++ game.py
@@ -0,0 +1,12 @@
+import random
+import copy
+import sys
+import pygame
+from constants import *
+from pygame.locals import *
+
+def main():
+    print(BOARD_WIDTH)
+
+if __name__ == '__main__':
+    main()
