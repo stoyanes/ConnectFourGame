d9a11080c4d31aa4d0e302a18830dc8c2ea515d6
Stoyan Stoyanov <djstoxer@gmail.com>
2013-01-02 21:37:35 +0200

    just regular commit. Nothing special

--- game.py
+++ game.py
@@ -3,10 +3,22 @@ import copy
 import sys
 import pygame
 from constants import *
+from functions import *
 from pygame.locals import *
 
 def main():
-    print(BOARD_WIDTH)
-
+    init_global_vars()
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption('Connect Four')
+    REDPILERECT = pygame.Rect(int(SPACE_SIZE / 2), WINDOW_HEIGHT - int(3 * SPACE_SIZE / 2), SPACE_SIZE, SPACE_SIZE)
+    BLACKPILERECT = pygame.Rect(WINDOW_WIDTH - int(3 * SPACESIZE / 2), WINDOW_HEIGHT - int(3 * SPACE_SIZE / 2), SPACE_SIZE, SPACE_SIZE)
+    REDTOKENIMG = pygame.image.load('4row_red.png')
+    REDTOKENIMG = pygame.transform.smoothscale(REDTOKENIMG, (SPACESIZE, SPACESIZE))
+    BLACKTOKENIMG = pygame.image.load('4row_black.png')
+    BLACKTOKENIMG = pygame.transform.smoothscale(BLACKTOKENIMG, (SPACESIZE, SPACESIZE))
+    BOARDIMG = pygame.image.load('4row_board.png')
+    BOARDIMG = pygame.transform.smoothscale(BOARDIMG, (SPACESIZE, SPACESIZE))
 if __name__ == '__main__':
     main()
